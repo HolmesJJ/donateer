69120602d31bc6beb598eb09d7c9da11cd29b0a7
HU JIAJUN <hujiajun19961024@gmail.com>
2022-12-18 17:14:10 +0800

    Update Login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -51,132 +51,131 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       body: SingleChildScrollView(
         child: SizedBox(
-      height: MediaQuery.of(context).size.height,
-        child: Padding(
-          padding: const EdgeInsets.all(22),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: <Widget>[
-                Image.asset('assets/images/auth_image.png',
-                    height: 200, fit: BoxFit.fitWidth),
-                Text('Welcome back.\nLogin to your account',
-                    style: Theme.of(context).textTheme.headline1),
-                const SizedBox(height: 30),
-                /*
-                if (defaultTargetPlatform == TargetPlatform.iOS)
-                  SignInWithAppleButton(
-                    onPressed: () async {
-                      final credential = await SignInWithApple.getAppleIDCredential(
-                        scopes: [
-                          AppleIDAuthorizationScopes.email,
-                          AppleIDAuthorizationScopes.fullName,
-                        ],
-                      );
-                      print(credential);
-                      // Now send the credential (especially `credential.authorizationCode`) to your server to create a session
-                      // after they have been validated with Apple (see `Integration` section for more information on how to do this)
+          height: MediaQuery.of(context).size.height,
+          child: Padding(
+            padding: const EdgeInsets.all(22),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  Image.asset('assets/images/auth_image.png',
+                      height: 200, fit: BoxFit.fitWidth),
+                  Text('Welcome back.\nLogin to your account',
+                      style: Theme.of(context).textTheme.headline1),
+                  const SizedBox(height: 30),
+                  /*
+                  if (defaultTargetPlatform == TargetPlatform.iOS)
+                    SignInWithAppleButton(
+                      onPressed: () async {
+                        final credential = await SignInWithApple.getAppleIDCredential(
+                          scopes: [
+                            AppleIDAuthorizationScopes.email,
+                            AppleIDAuthorizationScopes.fullName,
+                          ],
+                        );
+                        print(credential);
+                        // Now send the credential (especially `credential.authorizationCode`) to your server to create a session
+                        // after they have been validated with Apple (see `Integration` section for more information on how to do this)
+                      },
+                    ),
+                    */
+                  ElevatedButton.icon(
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.black, backgroundColor: Colors.white,
+                    ), 
+                    icon: FaIcon(FontAwesomeIcons.google, color: Colors.red[400],),
+                    label: const Text('Sign In with Google'), 
+                    onPressed: () {
+                      final provider = Provider.of<GoogleSignInProvider>(context, listen: false);
+                      provider.googleLogin().then((userCredential) => {
+                        // User exists and is a new Google user
+                          if (userCredential != null) {
+                            if (!userCredential.additionalUserInfo.isNewUser) {
+                            // Not a new Google user
+                            Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(
+                                builder: (context) => const TabsScreen(),
+                              ),
+                            ),
+                          } else {
+                              Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(
+                                builder: (context) => RegisterIncomeScreen(user: userCredential.user,),
+                              ),
+                            ),
+                          } 
+                        }
+                      });
                     },
                   ),
-                  */
-                ElevatedButton.icon(
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: Colors.black, backgroundColor: Colors.white,
-                  ), 
-                  icon: FaIcon(FontAwesomeIcons.google, color: Colors.red[400],),
-                  label: const Text('Sign In with Google'), 
-                  onPressed: () {
-                    final provider = Provider.of<GoogleSignInProvider>(context, listen: false);
-                    provider.googleLogin().then((userCredential) => {
-                      // User exists and is a new Google user
-                        if (userCredential != null) {
-                          if (!userCredential.additionalUserInfo.isNewUser) {
-                          // Not a new Google user
-                          Navigator.of(context).pushReplacement(
-                            MaterialPageRoute(
-                              builder: (context) => const TabsScreen(),
-                            ),
-                          ),
-                        } else {
-                            Navigator.of(context).pushReplacement(
-                            MaterialPageRoute(
-                              builder: (context) => RegisterIncomeScreen(user: userCredential.user,),
-                            ),
-                          ),
-                        } 
+                  const SizedBox(height: 15),
+                  const Text('— OR —'),
+                  const SizedBox(height: 12),
+                  const Text('Continue with your email'),
+                  TextFormField(
+                    key: const ValueKey('email'),
+                    validator: (value) {
+                      if (value!.isEmpty || !value.contains('@')) {
+                        return 'Please enter a valid email address.';
                       }
-                    });
-                  },
-                ),
-                const SizedBox(height: 15),
-                const Text('— OR —'),
-                const SizedBox(height: 12),
-                const Text('Continue with your email'),
-                TextFormField(
-                  key: const ValueKey('email'),
-                  validator: (value) {
-                    if (value!.isEmpty || !value.contains('@')) {
-                      return 'Please enter a valid email address.';
-                    }
-                    return null;
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: const InputDecoration(
-                    labelText: 'Email address',
+                      return null;
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: const InputDecoration(
+                      labelText: 'Email address',
+                    ),
+                    controller: _emailTextController,
                   ),
-                  controller: _emailTextController,
-                ),
-                TextFormField(
-                  key: const ValueKey('password'),
-                  validator: (value) {
-                    if (value!.isEmpty || value.length < 7) {
-                      return 'Password must be at least 7 characters long.';
-                    }
-                    return null;
-                  },
-                  decoration: const InputDecoration(labelText: 'Password'),
-                  obscureText: true,
-                  controller: _passwordTextController,
-                ),
-                const Spacer(),
-                ElevatedButton(
-                  child: const Text('LOGIN'),
-                  onPressed: () async {
-                    _trySubmit();
-                    User? user =
-                        await FirebaseAuth.instance.signInWithEmailAndPassword(
-                      email: _emailTextController.text,
-                      password: _passwordTextController.text,
-                    ).then((UserCredential userCredential) {return userCredential.user;});
-
-                    if (user != null) {
-                      Navigator.of(context).pushReplacement(
+                  TextFormField(
+                    key: const ValueKey('password'),
+                    validator: (value) {
+                      if (value!.isEmpty || value.length < 7) {
+                        return 'Password must be at least 7 characters long.';
+                      }
+                      return null;
+                    },
+                    decoration: const InputDecoration(labelText: 'Password'),
+                    obscureText: true,
+                    controller: _passwordTextController,
+                  ),
+                  const Spacer(),
+                  ElevatedButton(
+                    child: const Text('LOGIN'),
+                    onPressed: () async {
+                      _trySubmit();
+                      User? user =
+                          await FirebaseAuth.instance.signInWithEmailAndPassword(
+                        email: _emailTextController.text,
+                        password: _passwordTextController.text,
+                      ).then((UserCredential userCredential) {return userCredential.user;});
+                      if (user != null) {
+                        Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(
+                            builder: (context) => const TabsScreen(),
+                          ),
+                        );
+                      }
+                    },
+                  ),
+                  TextButton(
+                    child: const Text('Create an account',
+                        style: TextStyle(
+                          decoration: TextDecoration.underline,
+                        ),),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
                         MaterialPageRoute(
-                          builder: (context) => const TabsScreen(),
+                          builder: (context) => const RegisterDetailsScreen(),
                         ),
                       );
-                    }
-                  },
-                ),
-                TextButton(
-                  child: const Text('Create an account',
-                      style: TextStyle(
-                        decoration: TextDecoration.underline,
-                      ),),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const RegisterDetailsScreen(),
-                      ),
-                    );
-                  },
-                )
-              ],
+                    },
+                  )
+                ],
+              ),
             ),
           ),
         ),
-      ),
       ),
     );
   }
